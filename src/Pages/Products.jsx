fb9540d9b017249eb1611ad5f76a1ceac5f2a632
tarun-upadhyay <tarunu88@gmail.com>
2022-09-29 23:58:59 +0530

    starts

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,35 +1,39 @@
-import { useEffect } from "react"
-import { useState } from "react"
-import { useParams } from "react-router-dom"
-import data from "../db.json"
-import { Box ,Grid,Text, GridItem, Image} from "@chakra-ui/react"
-import { useContext } from "react"
-import { AppContext } from "../AuthContext/AuthcontextProvider"
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import data from "../db.json";
+import { Box, Grid, Text, Rating, GridItem, Image } from "@chakra-ui/react";
+import { useContext } from "react";
+import { AppContext } from "../AuthContext/AuthcontextProvider";
+import { StarIcon } from "@chakra-ui/icons";
+export default function Products() {
+  const { state, dispatch } = useContext(AppContext);
 
-export default function Products(){
-const {state, dispatch } = useContext(AppContext)
-
-   
-    return (
-        <>
+  return (
+    <>
+      <Box>
         <Box>
-            <Box>
-                <Grid templateColumns="repeat(4,1fr)" gap={5}>
-                    {
-                        state.data?.map((el)=>
-                             <GridItem>
-                                <Image src={el.image}/>
-                                <Box>
-                                    <Text>{el.title}</Text>
-                                </Box>
-                             </GridItem>
-                        )
-                    }
-
-                </Grid>
-            </Box>
-
+          <Grid templateColumns="repeat(4,1fr)" gap={5}>
+            {state.data?.map((el) => (
+              <GridItem>
+                <Image src={el.image} />
+                <Box>
+                  <Text>{el.title}</Text>
+                  <Text>{el.name}</Text>
+                  <Box>
+                    {new Array(5).fill("").map((_, i) => (
+                      <StarIcon
+                        key={i}
+                        color={i < el.stars ? "black.500" : "gray.300"}
+                      />
+                    ))}
+                  </Box>
+                </Box>
+              </GridItem>
+            ))}
+          </Grid>
         </Box>
-        </>
-    )
+      </Box>
+    </>
+  );
 }
