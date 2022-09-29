411b9dded0d15f01b29d5f039b1f31f577edb299
tarun-upadhyay <tarunu88@gmail.com>
2022-09-29 17:41:33 +0530

    nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,6 +22,7 @@ import {
   Flex,
   Tooltip,
   Spacer,
+  Container,
 } from "@chakra-ui/react";
 import styles from "./Navbar.module.css";
 import {
@@ -32,6 +33,8 @@ import {
   AtSignIcon,
 } from "@chakra-ui/icons";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { } from '@fortawesome/fontawesome-free-solid'
 export function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
@@ -150,10 +153,56 @@ export function Navbar() {
                   <Text>Sign In</Text>
                   <Text fontSize="xs">for FREE Shipping</Text>
                 </Box>
+                <Container>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 800">
+                    <path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64v48H160V112zm-48 48H48c-26.5 0-48 21.5-48 48V416c0 53 43 96 96 96H352c53 0 96-43 96-96V208c0-26.5-21.5-48-48-48H336V112C336 50.1 285.9 0 224 0S112 50.1 112 112v48zm24 96c-13.3 0-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24s-10.7 24-24 24zm200-24c0 13.3-10.7 24-24 24s-24-10.7-24-24s10.7-24 24-24s24 10.7 24 24z" />
+                  </svg>
+                </Container>
               </Flex>
-              <FontAwesomeIcon icon={fasolidfabagshopping} />
             </Box>
           </Flex>
+        </Box>
+        <Box>
+          <Box  bg="black" p="15px">
+            <Flex gap="42px" w="80%" m="auto" > 
+              <Box>
+                <Text fontSize="sm" color="white" >New</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" > Brands</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Makeup</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Skincare</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Hair</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Fragrance</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Tools & Brushes</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Bath & Body</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Mini Size</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Gifts</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Beauty Under $20</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" color="white" >Sale & Offers</Text>
+              </Box>
+            </Flex>
+          </Box>
         </Box>
       </Box>
     </>
